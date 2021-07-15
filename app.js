7e2bb58666eaef281bd76cce25770a4c129c7b48
Ryan P <rdpinkstaff@gmail.com>
2021-07-14 18:50:56 -0600

    Added event listeners and made x's display on board

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const gameBoard = (function () {
-    const board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    const board = ['', '', '', '', '', '', '', '', ''];
+    let boardGame = document.querySelectorAll('.boardSquare');
 
     function renderBoard() {
-        let boardGame = document.querySelectorAll('.boardSquare');
         for (let boards of boardGame) {
             let p = document.createElement('p');
             p.innerText = board[boards.dataset.index];
             boards.appendChild(p);
         }
     }
+    function clearBoard() {
+        for (let boards of boardGame) {
+            while (boards.firstChild) {
+                boards.removeChild(boards.firstChild);
+            }
+        }
+    }
+    function playerClick() {
+        for (let boards of boardGame) {
+            boards.addEventListener('click', (e) => {
+                board.splice(boards.dataset.index, 1, "x");
+                console.log(board);
+                clearBoard();
+                renderBoard();  
+            })
+        }
+       
+    }
     return {
         renderBoard,
+        playerClick,
+       
     }
 })();
+
+const Player = function (name, symbol) {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+
+
+}
