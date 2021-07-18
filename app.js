b2aec55bc80ef1c2ff61e81c6d1ff10d2c72b8df
Ryan P <rdpinkstaff@gmail.com>
2021-07-18 16:34:51 -0600

    Game working and just need to make pretty

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const player = function (name, symbol) {
-    
-    return { name, symbol};
+
+    return { name, symbol };
 }
 
 
@@ -18,23 +18,26 @@ const gameBoard = (function () {
         for (let boards of boardGame) {
             let image = document.createElement('img');
 
-            if(board[boards.dataset.index] === 'x'){
+            if (board[boards.dataset.index] === 'x') {
                 image.src = 'close.png';
             }
-            else if(board[boards.dataset.index] === 'o'){
+            else if (board[boards.dataset.index] === 'o') {
                 image.src = 'o-symbol.png';
             }
-        
+
             boards.appendChild(image);
         }
     }
-    function playerWins(winner){
+    function playerWins(winner) {
         let p = document.createElement('p');
-        p.setAttribute('id','winningPlayer');
+        p.setAttribute('id', 'winningPlayer');
         p.innerText = `Congrats ${winner} wins!`;
+        if (winner === 'draw') {
+            p.innerText = `It's a draw, play again.`
+        }
         container.appendChild(p);
     }
-    function clearPlayerWins(){
+    function clearPlayerWins() {
         let winPlayer = document.querySelector('#winningPlayer');
         winPlayer.parentNode.removeChild(winPlayer);
     }
@@ -45,95 +48,119 @@ const gameBoard = (function () {
             }
         }
     }
+
     function playerClick() {
-        for (let boards of boardGame) {
-            boards.addEventListener('click', (e) => {
-                let index = boards.dataset.index;
-                if (count % 2 === 0 && board[index] === '') {
-                    board.splice(index, 1, player1.symbol);
-                    count++;
-                }
-                else if (count % 2 === 1 && board[index] === '') {
-                    board.splice(index, 1, player2.symbol);
-                    count++;
-                }
-                clearBoard();
-                renderBoard();
-                if (count > 4) {
-                    checkWinner(count);
-                }
-                console.log(count);
-            })
+        if (winner === '') {
+            for (let boards of boardGame) {
+                boards.addEventListener('click', addSymbols)
+            }
         }
     }
+    function addSymbols(e) {
+        let index = e.target.dataset.index;
+        if (count % 2 === 0 && board[index] === '') {
+            board.splice(index, 1, player1.symbol);
+            count++;
+        }
+        else if (count % 2 === 1 && board[index] === '') {
+            board.splice(index, 1, player2.symbol);
+            count++;
+        }
+        clearBoard();
+        renderBoard();
+        if (count > 4) {
+            checkWinner(count);
+            removeClick();
+        }
+
+    }
+    function removeClick(e) {
+        if (winner !== '') {
+            for (let boards of boardGame) {
+                boards.removeEventListener('click', addSymbols);
+            }
+        }
+    }
+
     function checkWinner(count) {
-        
-            if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
-                console.log(`${board[0]} wins!`);
-                winner = board[0];
-            }
-            else if (board[3] === board[4] && board[4] === board[5] && board[3] !== '') {
-                console.log(`${board[3]} wins!`);
-                winner = board[3];
-            }
-            else if (board[6] === board[7] && board[7] === board[8] && board[6] !== '') {
-                console.log(`${board[6]} wins!`);
-                winner = board[6];
-            }
-            else if (board[0] === board[3] && board[3] === board[6] && board[0] !== '') {
-                console.log(`${board[0]} wins!`);
-                winner = board[0];
-            }
-            else if (board[1] === board[4] && board[4] === board[7] && board[1] !== '') {
-                console.log(`${board[1]} wins!`);
-                winner = board[1];
-            }
-            else if (board[2] === board[5] && board[5] === board[8] && board[2] !== '') {
-                console.log(`${board[2]} wins!`);
-                winner = board[2];
-            }
-            else if (board[0] === board[4] && board[4] === board[8] && board[0] !== '') {
-                console.log(`${board[0]} wins!`);
-                winner = board[0];
-            }
-            else if (board[2] === board[4] && board[4] === board[6] && board[2] !== '') {
-                console.log(`${board[2]} wins!`);
-                winner = board[2];
-            }
-            else if (count === 9) {
-                console.log(`It's a draw!`);
-            }
-            if(winner === player1.symbol){
-                winner = player1.name;
-                playerWins(winner);
-                console.log('Player 1 wins!');
-            }
-            else if(winner === player2.symbol){
-                winner = player2.name;
-                playerWins(winner);
-                console.log('Player 2 wins');
-            }
-                    
-        restartGame();
+
+        if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
+            console.log(`${board[0]} wins!`);
+            winner = board[0];
+        }
+        else if (board[3] === board[4] && board[4] === board[5] && board[3] !== '') {
+            console.log(`${board[3]} wins!`);
+            winner = board[3];
+        }
+        else if (board[6] === board[7] && board[7] === board[8] && board[6] !== '') {
+            console.log(`${board[6]} wins!`);
+            winner = board[6];
+        }
+        else if (board[0] === board[3] && board[3] === board[6] && board[0] !== '') {
+            console.log(`${board[0]} wins!`);
+            winner = board[0];
+        }
+        else if (board[1] === board[4] && board[4] === board[7] && board[1] !== '') {
+            console.log(`${board[1]} wins!`);
+            winner = board[1];
+        }
+        else if (board[2] === board[5] && board[5] === board[8] && board[2] !== '') {
+            console.log(`${board[2]} wins!`);
+            winner = board[2];
+        }
+        else if (board[0] === board[4] && board[4] === board[8] && board[0] !== '') {
+            console.log(`${board[0]} wins!`);
+            winner = board[0];
+        }
+        else if (board[2] === board[4] && board[4] === board[6] && board[2] !== '') {
+            console.log(`${board[2]} wins!`);
+            winner = board[2];
+        }
+        else if (count === 9) {
+            console.log('No winner')
+        }
+
+        if (winner === player1.symbol) {
+            winner = player1.name;
+            playerWins(winner);
+        }
+        else if (winner === player2.symbol) {
+            winner = player2.name;
+            playerWins(winner);
+        }
+        else if (count === 9) {
+            winner = 'draw';
+            playerWins(winner);
+        }
+
     }
     function restartGame() {
         const restart = document.querySelector('#restartGame');
 
-        restart.addEventListener('click', function (e) {
+
+        restart.addEventListener('mousedown', function (e) {
             clearBoard();
-            //clearPlayerWins();
             for (let i = 0; i < board.length; i++) {
                 board[i] = '';
             }
-            winner = false;
+
+            if (winner !== '') {
+                clearPlayerWins();
+            }
+            winner = '';
             count = 0;
+            playerClick();
+            e.stopPropagation();
+            e.preventDefault();
         })
-        
+
     }
     playerClick();
+    restartGame();
+
     return {
         checkWinner,
-        restartGame,
+
 
     }
 })();
